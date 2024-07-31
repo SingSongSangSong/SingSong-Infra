7dfbc4d0e34239ac0ace2c452a5f67a8ffe4360e
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-07-31 16:54:49 +0900

    Feat: add loadbalancer

---
+++ loadbalancer_security_group.tf
@@ -0,0 +1,33 @@
+// Security Group 생성
+resource "aws_security_group" "singsong_security_group" {
+  vpc_id = aws_vpc.singsong_vpc.id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
