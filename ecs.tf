431eabe5fc62214aee478c5083f54f44276e9190
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-07-31 16:54:58 +0900

    Feat: add ecs

---
+++ ecs.tf
@@ -0,0 +1,119 @@
+// ECS Cluster 생성
+resource "aws_ecs_cluster" "singsong_ecs_cluster" {
+  name = var.ecs_cluster_name
+}
+
+// IAM Role 생성
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole2"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+// ECS Task Definition 생성
+resource "aws_ecs_task_definition" "singsong_ecs_task_definition" {
+  family                   = "singsong-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "1024"
+  memory                   = "2048"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = jsonencode([
+    {
+      name      = "singsong-container"
+      image     = "${aws_ecr_repository.singsong_golang_ecr_repository.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        },
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    },
+    {
+      name      = "datadog-agent"
+      image     = "datadog/agent:latest"
+      essential = true
+      environment = [
+        {
+          name  = "DD_API_KEY"
+          value = var.datadog_api_key
+        },
+        {
+          name  = "DD_SITE"
+          value = var.datadog_url
+        },
+        {
+          name  = "ECS_FARGATE"
+          value = "true"
+        },
+        {
+          name  = "DD_RUNTIME_SECURITY_CONFIG_ENABLED"
+          value = "true"
+        },
+        {
+          name  = "DD_RUNTIME_SECURITY_CONFIG_EBPFLESS_ENABLED"
+          value = "true"
+        }
+      ],
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/singsong"
+          "awslogs-region"        = "ap-northeast-2"
+          "awslogs-stream-prefix" = "datadog-agent"
+        }
+      },
+      healthCheck = {
+        command     = ["CMD-SHELL", "/probe.sh"]
+        interval    = 30
+        timeout     = 5
+        retries     = 2
+        startPeriod = 60
+      }
+    }
+  ])
+}
+
+// ECS Service 생성
+resource "aws_ecs_service" "singsong_ecs_service" {
+  name                   = "singsong-ecs-service"
+  cluster                = aws_ecs_cluster.singsong_ecs_cluster.id
+  task_definition        = aws_ecs_task_definition.singsong_ecs_task_definition.arn
+  desired_count          = 2
+  launch_type            = "FARGATE"
+  scheduling_strategy    = "REPLICA"
+  health_check_grace_period_seconds = 120
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.singsong_target_group.arn
+    container_name = "singsong-container"
+    container_port   = 8080
+  }
+
+  network_configuration {
+    subnets         = [aws_subnet.singsong_public_subnet1.id, aws_subnet.singsong_public_subnet2.id]
+    security_groups = [aws_security_group.singsong_security_group.id]
+    assign_public_ip = true
+  }
+}
